1a2bc44fe87f5efa850101be1deb4bb85c2eca27
nidhimshaji <42461490+nidhimshaji@users.noreply.github.com>
2019-08-30 00:28:48 -0700

    more updates

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contacts";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Post from "./components/Post";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/:post_id" component={Post} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/:post_id" component={Post} />
+      </Switch>
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 const Post = props => {
   //console.log("props", props);
   console.log("hello");
-  const [id, setId] = useState(null);
+  const [indiPost, setindiPost] = useState([]);
 
   useEffect(() => {
-    setId(props.match.params.post_id);
-  }, [id]);
+    const getPost = async () => {
+      let id = props.match.params.post_id;
+      try {
+        const res = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        setindiPost(res.data);
+      } catch (e) {
+        console.log("error", e);
+      }
+    };
 
+    getPost();
+  }, []);
+  console.log("indi post", indiPost);
   return (
     <div>
       <h1>Post Page</h1>
-      {id}
+      {indiPost.title}
+      {indiPost.title}
     </div>
   );
 };
